f1618fea3bd4a1f5cb9b1688e0a59ca0d4e9fa25
Kirill Kosenko <kirill@kosenko.pro>
2023-06-12 19:25:13 +0300

    lesson-01

---
+++ resources/views/news.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новости</title>
+</head>
+<body>
+<h1>Новости</h1>
+<menu>
+    <ul>
+        <li><a href="/">Главная</a></li>
+        <li><a href="/about">О проекте</a></li>
+        <li><a href="/news">Новости</a></li>
+    </ul>
+</menu>
+</body>
+</html>
